aa4037303a0867fea5b5326195414f89b5101f35
nssafir <nssafir@gmail.com>
2020-08-10 22:46:43 -0500

    Updates

---
+++ precache-manifest.4b5c6bb375b5f4af7a225500a7ede3ba.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4bec03dff17ae36adf6e4caf3e88891",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "26a5b956511e56875042",
+    "url": "/website/static/css/2.06c478a6.chunk.css"
+  },
+  {
+    "revision": "d821a93024eae78f0437",
+    "url": "/website/static/css/main.9ada33e5.chunk.css"
+  },
+  {
+    "revision": "26a5b956511e56875042",
+    "url": "/website/static/js/2.5720fe58.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/website/static/js/2.5720fe58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d821a93024eae78f0437",
+    "url": "/website/static/js/main.96c40cf9.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  },
+  {
+    "revision": "8832683144e1b0bfc7a0bc3958a7daae",
+    "url": "/website/static/media/chopped.88326831.jpeg"
+  },
+  {
+    "revision": "c431a19b71a8e705d98447beabcc2a98",
+    "url": "/website/static/media/gov.c431a19b.jpg"
+  },
+  {
+    "revision": "92a9e76884f4a318343247020bbb34a9",
+    "url": "/website/static/media/hockey.92a9e768.jpg"
+  },
+  {
+    "revision": "cbcc6c44577802ab79ee3a4bfc6350fc",
+    "url": "/website/static/media/me.cbcc6c44.jpg"
+  },
+  {
+    "revision": "f28cb0a316f1deee12a209602b3e830c",
+    "url": "/website/static/media/oxford.f28cb0a3.png"
+  },
+  {
+    "revision": "c07256737d947a88817ff6de38210611",
+    "url": "/website/static/media/qrg.c0725673.png"
+  },
+  {
+    "revision": "1638952189626f7d4b9e6949fb60f452",
+    "url": "/website/static/media/resume.16389521.pdf"
+  },
+  {
+    "revision": "68a17daddc6a5d9f314fc1a9c70b339e",
+    "url": "/website/static/media/uga.68a17dad.png"
+  }
+]);
